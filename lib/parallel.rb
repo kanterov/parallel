2121e778e237fd07ab1718fbb4468fdb434f9d00
Gleb Kanterov <gleb@kanterov.ru>
2012-10-06 22:16:38 +0700

    * refactoring

--- lib/parallel.rb
+++ lib/parallel.rb
@@ -1,134 +1,37 @@
 module Parallel
     DEFAULT_THREADS_COUNT = 2
 
-    def self.parallel(count=DEFAULT_THREADS_COUNT)
+    def self.parallel(array, count=DEFAULT_THREADS_COUNT)
         raise ArgumentError, "Illegal arguments!" if count == 0
 
-        out = []
-        threads = (0..count-1).map { |i|
-            Thread.new do
-                result = yield(i)
+        array = array.to_a
 
-                Thread.exclusive { out[i] = result }
+        return [] if array.empty?
+
+        partitions = array.each_slice((array.length + count - 1) / count).to_a
+        threads = (0..count-1).map {|i|
+            Thread.new do
+                yield(partitions[i] || [])
             end
         }
-        
-        join_threads(threads)
 
-        out.flatten(1)
-    end
-
-    def self.join_threads(threads)
-        threads.compact.each do |thread|
-            begin
-                thread.join
-            rescue Interrupt
-                # ok, wait for others
-            end
-        end
+        threads.compact.map {|thread| thread.value}.flatten(1)
     end
 
     def self.map(array, count=DEFAULT_THREADS_COUNT, &block)
-        raise ArgumentError, "Illegal arguments!" if count == 0
-
-        # to support enumerables
-        array = array.to_a
-
-        # don't do any work for empty arrays
-        return [] if array.empty?
-
-        partitions = partition_per_thread_with_index(array, count)
-
-        out = parallel(count) do |i|
-            partition = partitions[i] || []
-
-            partition.map do |value,index|
-                args = [value]
-                value = block.call(*args)
-
-                [value, index]
-            end
-        end
-
-        remove_index(out)
+        parallel(array, count) {|partition| partition.map &block}
     end
 
     def self.select(array, count=DEFAULT_THREADS_COUNT, &block)
-        raise ArgumentError, "Illegal arguments!" if count == 0
-
-        # to support enumerables
-        array = array.to_a
-
-        # don't do any work for empty arrays
-        return [] if array.empty?
-
-        partitions = partition_per_thread_with_index(array, count)
-
-        out = parallel(count) do |i|
-            partition = partitions[i] || []
-
-            partition.select do |value,index|
-                args = [value]
-                block.call(*args)
-            end
-        end
-
-        remove_index(out)
+        parallel(array, count) {|partition| partition.select &block}
     end
 
     def self.any?(array, count=DEFAULT_THREADS_COUNT, &block)
-        raise ArgumentError, "Illegal arguments!" if count == 0
-
-        # to support enumerables
-        array = array.to_a
-
-        # don't do any work for empty arrays
-        return false if array.empty?
-
-        partitions = partition_per_thread(array, count)
-
-        out = parallel(count) do |i|
-            partition = partitions[i] || []
-
-            partition.any? &block
-        end
-
-        out.any?
+        parallel(array, count) {|partition| partition.any? &block}.any?
     end
 
     def self.all?(array, count=DEFAULT_THREADS_COUNT, &block)
-        raise ArgumentError, "Illegal arguments!" if count == 0
-
-        # to support enumerables
-        array = array.to_a
-
-        # don't do any work for empty arrays
-        return true if array.empty?
-
-        partitions = partition_per_thread(array, count)
-
-        out = parallel(count) do |i|
-            partition = partitions[i] || []
-
-            partition.all? &block
-        end
-
-        out.all?
-    end
-
-    # returns array of enumerables
-    def self.partition_per_thread_with_index(array, count)
-        array.each_with_index.each_slice((array.length + count - 1) / count).to_a
-    end
-
-    # returns array of enumerables
-    def self.partition_per_thread(array, count)
-        array.each_slice((array.length + count - 1) / count).to_a
-    end
-
-    def self.remove_index(out)
-        out.sort_by {|value,index| index}
-           .map     {|value,index| value}
+        parallel(array, count) {|partition| partition.all? &block}.all?
     end
 
 end
